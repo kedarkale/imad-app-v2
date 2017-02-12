042624ad208ac320e9ec24e500742968f6faf3e5
kedarkale <kedar30092010@gmail.com>
2017-02-12 19:15:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 var myimg = document.getElementById('image');
 
 myimg.onclick = function() {
-    myimg.style.marginLeft="100px";
+    myimg.style.marginLeft=myimg.style.marginLeft + "100px";
 };
